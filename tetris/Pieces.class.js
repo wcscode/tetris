702956ca4030a93404dac7837df0f9c58025ad6c
wcscode <67437524+wcscode@users.noreply.github.com>
2020-09-22 18:06:59 -0300

    Add files via upload

--- tetris/Pieces.class.js
+++ tetris/Pieces.class.js
@@ -3,73 +3,141 @@ export default class Piece{
     constructor(game, blockSide){
         
         this._game = game;
-        this._blockSide = blockSide;
+        this._blockSide = blockSide;      
 
-        const o =  [
-            0, 0, 0, 0,
-            0, 1, 1, 0,
-            0, 1, 1, 0,
-            0, 0, 0, 0
-        ];
+        const i = { blocks: [
 
-        const i =  [
-            0, 0, 1, 0,
-            0, 0, 1, 0,
-            0, 0, 1, 0,
-            0, 0, 1, 0
-        ];
+                0, 0, 0, 0,
+                1, 1, 1, 1,
+                0, 0, 0, 0,
+                0, 0, 0, 0
+            ],
+            
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }
 
-        const z =  [
-            0, 0, 1, 0,
-            0, 1, 1, 0,
-            0, 1, 0, 0,
-            0, 0, 0, 0
-        ];
+        const t = { blocks: [
 
-        const t =  [
-            0, 0, 1, 0,
-            0, 1, 1, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 0
-        ];
+                0, 0, 0,
+                1, 1, 1,
+                0, 1, 0            
+            ],
 
-        const l =  [
-            0, 0, 1, 0,
-            0, 0, 1, 0,
-            0, 1, 1, 0,
-            0, 0, 0, 0
-        ];
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }
+
+        const l = { blocks: [
+
+                0, 0, 0,
+                1, 1, 1,
+                1, 0, 0           
+            ],
+
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }
+
+        const j = { blocks: [
+
+                0, 0, 0,
+                1, 1, 1,
+                0, 0, 1           
+            ],
+
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }
+
+        const s = { blocks: [
+
+                0, 0, 0,
+                0, 1, 1,
+                1, 1, 0            
+            ],
+
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }   
+
+
+        const z = { blocks: [
+
+                0, 0, 0,
+                1, 1, 0,
+                0, 1, 1            
+            ],
+
+            direction: 'UP',
+            x: this._blockSide * 8, 
+            y: 0,
+            canFall: true
+        }   
+
+        const o =  { blocks: [
+
+                1, 1,
+                1, 1            
+            ],
+
+            direction: 'UP',
+            x: this._blockSide * 9, 
+            y: 0,
+            canFall: true
+        };
              
         this._pieces = [];
 
-        this._pieces.push(o);
+        
         this._pieces.push(i);
-        this._pieces.push(z);
         this._pieces.push(t);
         this._pieces.push(l); 
+        this._pieces.push(j); 
+        this._pieces.push(s); 
+        this._pieces.push(z);
+        this._pieces.push(o);
 
         this._tickMovement = 0;
     }
 
     new = () => {
 
-        this._currentPiece = {
-            blocks: this._pieces[Math.floor((Math.random() * 4))],
+        this._currentPiece = this._pieces[6];
+       /* this._currentPiece = {
+            blocks: this._pieces[0], //this._pieces[Math.floor((Math.random() * 4))],
             direction: 'UP',
             x: this._blockSide * 8, 
             y: 0,
             canFall: true
-        }
+        }*/
     }
 
     moveDown = () => { this._currentPiece.y += this._blockSide; }
     moveLeft = () => { this._currentPiece.x += -this._blockSide; }
     moveRight = () => { this._currentPiece.x += this._blockSide; }
     rotate = () => {
+        
+        const positions = [
 
-        const positions = ['UP', 'LEFT', 'DOWN', 'RIGHT'];
+            {key:0, value:'UP'},
+            {key:1, value:'LEFT'},
+            {key:2, value:'DOWN'},
+            {key:3, value:'RIGHT'}
+        ];
 
-        const direction = this._currentPiece.direction;
+        const key = (positions.find(f => f.value == this._currentPiece.direction).key + 1) % 4;       
+        this._currentPiece.direction = positions.find(f => f.key == key).value;       
     }
 
     get canFall() { return this._currentPiece.canFall }
@@ -80,14 +148,17 @@ export default class Piece{
         if(!this.canFall)
             this.new();              
         
-        if (this._game.control.isPressedWithRate('PLAYER_1', 'RIGHT', 2, dt))
+        if (this._game.control.isPressedWithRate('PLAYER_1', 'UP', 8, dt))
             this.rotate();
 
-        if (this._game.control.isPressedWithRate('PLAYER_1', 'RIGHT', 2, dt))
+        if (this._game.control.isPressedWithRate('PLAYER_1', 'RIGHT', 4, dt))
             this.moveRight();
 
-        if (this._game.control.isPressedWithRate('PLAYER_1', 'LEFT', 2, dt))
-            this.moveLeft();      
+        if (this._game.control.isPressedWithRate('PLAYER_1', 'LEFT', 4, dt))
+            this.moveLeft(); 
+            
+        if (this._game.control.isPressedWithRate('PLAYER_1', 'DOWN', 4, dt))
+            this.moveDown();     
         
     }
 
@@ -96,38 +167,32 @@ export default class Piece{
         this._game.context.beginPath();
         this._game.context.fillStyle = 'red';
 
-        for(let y = 0; y < 4; ++y){
+        const size = Math.sqrt(this._currentPiece.blocks.length);
 
-            for(let x = 0; x < 4; ++x){
+        for(let y = 0; y < size; ++y){
+            
+            for(let x = 0; x < size; ++x){
 
                 let direction = this._currentPiece.direction;
-                let block = this._currentPiece.blocks[this.position(direction, {x, y})];
-                if(block == 1)
-                    this._game.context.fillRect(this._currentPiece.x + this._blockSide * x, this._currentPiece.y + this._blockSide * y, this._blockSide, this._blockSide);
-            }
+                let block = this._currentPiece.blocks[this.position(direction, size, x, y)];               
+                if(block == 1)                   
+                    this._game.context.fillRect(this._currentPiece.x + this._blockSide * x, this._currentPiece.y + this._blockSide * y, this._blockSide, this._blockSide);                
+            }            
         }
-       
-
     }
 
+    position = (direction, size, x, y) => {
 
-    //0  i = y * w + x
-    //90 i = 12 + y - (x * 4)
-    //180 i = 15 - ( x * 4) - x
-    //270 i = 13 + y  *(x * 4)
-    position = (direction, vectorPosition) => {
-
-        const { x, y } = vectorPosition;
-
+       
         switch(direction) { 
-            case 'LEFT':
-                return 12 + y - (x * 4)
-            case 'DOWN':
-                return 15 - ( x * 4) - x;
             case 'RIGHT':
-                return 13 + y  *(x * 4);
+                return  6 + y  - (x * size);
+            case 'DOWN':
+                return 8 - (y * size) - x;
+            case 'LEFT':
+                return  2 - y + (x * size)
             default:
-                return y * 4 + x;
+                return y * size + x;
         }
     }  
 }
